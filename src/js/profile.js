e242dd08ec066cfb22db5ee46e432b6fa0716c79
mohamed7855 <martm7726@gmail.com>
2025-01-02 02:01:39 +0200

    userProfie data from API

--- src/js/profile.js
+++ src/js/profile.js
@@ -12,21 +12,32 @@ window.addEventListener("scroll", function () {
   }
 });
 
-const baseURL = "https://jsonplaceholder.typicode.com";
+const baseURL = "https://linked-posts.routemisr.com";
 
-(function getUserData(userId) {
-  fetch(`${baseURL}/users/${userId}`)
+(function getUserData() {
+  fetch(`${baseURL}/users/profile-data`, {
+    headers: {
+      token: localStorage.getItem("token"),
+    },
+  })
     .then((res) => res.json())
     .then((data) => {
+      console.log(data);
       setData(data);
     });
-})("1");
+})();
 
 function setData(userData) {
   document.querySelectorAll(".userName").forEach((ele) => {
-    ele.innerHTML = userData.name || "Mohamed Omara";
+    ele.innerHTML = userData.user.name;
   });
-  document.querySelector(".address").innerHTML = `${
-    userData?.address?.street || "El Shohada"
-  }, ${userData?.address?.city || "Al Minufiyah, Egypt"}`;
+  document.querySelector(".userType").innerHTML = userData.user.gender;
+  document.querySelector(".dob").innerHTML = userData.user.dateOfBirth;
+  document.querySelectorAll(".userPhoto img").forEach((ele) => {
+    ele.src = userData.user.photo;
+  });
 }
+
+document.querySelector(".backButton").addEventListener("click", () => {
+  history.back();
+});
